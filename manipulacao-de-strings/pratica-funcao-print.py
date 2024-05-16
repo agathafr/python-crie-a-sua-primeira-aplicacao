03db9f3e85456557d7a7636bf3b1849162e1fcc0
Agatha <85461130+agathafr@users.noreply.github.com>
2024-05-16 18:26:10 -0300

    Create pratica-funcao-print.py
    Diferentes formas de formatar strings para exibição

---
+++ manipulacao-de-strings/pratica-funcao-print.py
@@ -0,0 +1,8 @@
+print('Python na Escola de Programação da Alura')
+
+nome = input ('Qual é o seu nome?')
+idade = input ('Qual é a sua idade?')
+
+print(f'Meu nome é {nome} e tenho {idade} anos')
+
+print('A', 'L', 'U', 'R', 'A', sep='\n')
